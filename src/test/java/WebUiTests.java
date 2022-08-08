dcacbc64e533b10d35e012fac7e99bfd13e61ddc
bolshih <artem.bolshih@mail.ru>
2022-08-08 18:35:00 +0300

    CI build failed

--- src/test/java/WebUiTests.java
+++ src/test/java/WebUiTests.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class Webuitest {
+public class WebUiTests {
 
 
     @Test
