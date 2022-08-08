ff94a9d912641473df12bb691bb720ac404b8985
bolshih <artem.bolshih@mail.ru>
2022-08-08 13:08:56 +0300

    отправка формы с валидными значениями

--- src/test/java/webuitest.java
+++ src/test/java/webuitest.java
@@ -1,2 +1,19 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 public class webuitest {
+
+    @Test
+    void shouldTestWebUi(){
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Сергей Петров");
+        form.$("[data-test-id=phone] input").setValue("+77653984367");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
